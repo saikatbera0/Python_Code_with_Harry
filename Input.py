20f236b27fae05b9d9ee7a38620447df5d3e53ba
Saikat Bera <avisaikat7029@gmail.com>
2024-07-30 16:59:57 +0530

    add adress on input file

--- Input.py
+++ Input.py
@@ -3,3 +3,5 @@ print("My name is ",name)
 # Take int value as Input
 age=int(input("Enter your age:-> "))
 print(name,"'s age is ",age)
+adress=input("Enter your adress:->")
+print("My adress is:",adress)
