3c2db3001b1bcd9b69e3651df9f781002f952154
Saikat Bera <avisaikat7029@gmail.com>
2024-07-31 11:50:50 +0530

    This is about String part 1 and video no 11

---
+++ String1.py
@@ -0,0 +1,16 @@
+Name = "Saikat"
+print(Name)
+# Concadination
+print(Name+" Bera")
+# Inexing
+print(Name[0])
+print(Name[1])
+print(Name[2])
+print(Name[3])
+print(Name[4])
+print(Name[5])
+# print(Name[6]) #It is throughs an error becouse this index is not exis
+# looping string
+print("Usiing for loop: ")
+for char in Name:
+    print(char)
