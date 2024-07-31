3d990948e4a58133062a982095b3d7d096ec4181
Saikat Bera <avisaikat7029@gmail.com>
2024-07-31 12:55:57 +0530

    This code About Sliceing of string part 2 and video 12

---
+++ String2.py
@@ -0,0 +1,16 @@
+Name="Saikat,Disha"
+print(Name[0:6]) # Output =>Saikat #
+# To find lenght of a string use len() function 
+print("Lenght of my Name",len(Name))
+
+
+# Sliceing
+print("Lenght of Saikat",len(Name[0:6])) #Output=>6
+
+#Negative sliceing
+print(Name[0:-3]) #Output=> Saikat,Di
+
+# print(Name[-1:-6]) #Output=>ng
+
+#Quick Quiz
+print(Name[-4:-2])
